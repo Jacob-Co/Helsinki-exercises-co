2c340a6880ba49db863ec8f50bfef46c175789f5
Jacobtco <jacobtco@gmail.com>
2020-08-16 17:21:30 +0800

    Finished exercises 2.6-2.10

--- part2/course-information/src/index.js
+++ part2/course-information/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Course from './Components/Course';
 
-
 const App = () => {
   const courses = [
     {
---
+++ part2/phonebook/src/components/App.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+
+const Persons = ({persons}) => {
+  return (
+    <>
+      {persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
+    </>
+  )
+}
+
+const PersonForm = ({newName, newNumber, handleOnNameChange, handleOnNumberChange, handleFormSubmit}) => {
+  return (
+    <form onSubmit={handleFormSubmit}>
+    <div>
+      name: <input value={newName} type="text" onChange={handleOnNameChange}/>
+    </div>
+    <div>
+      number: <input value={newNumber} type="text" onChange={handleOnNumberChange}/>
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+  )
+}
+
+const Filter = ({filter, handleOnChange}) => {
+  return (
+    <div>
+      filter contacts by name: <input value={filter} onChange={handleOnChange}/>
+    </div>
+  )
+}
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]);
+  const [ newName, setNewName ] = useState('');
+  const [ newNumber, setNewNumber ] = useState('');
+  const [ filter, setFilter ] = useState('');
+
+  const personsToShow = (filter === '') 
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()));
+
+  const duplicateName = (name) => {
+   return !!(persons.find(person => person.name.toLowerCase() === name.toLowerCase()));
+  }
+
+  const handleNameChange = (e) => {
+    setNewName(e.target.value);
+  }
+
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
+  }
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  }
+
+  const addPerson = (e) => {
+    e.preventDefault();
+    if (duplicateName(newName)) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      setPersons(persons.concat({ name: newName, number: newNumber }));
+      setNewName('');
+      setNewNumber('');
+    }
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filter={filter} handleOnChange={handleFilterChange}/>
+      <h3>Add a new Contact</h3>
+      <PersonForm 
+        newName={newName} 
+        newNumber={newNumber} 
+        handleFormSubmit={addPerson}
+        handleOnNameChange={handleNameChange}
+        handleOnNumberChange={handleNumberChange}
+      />
+      <h3>Contacts</h3>
+      <Persons persons={personsToShow}/>
+    </div>
+  )
+}
+
+export default App
